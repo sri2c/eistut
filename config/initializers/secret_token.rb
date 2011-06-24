4fa932247d4ea5f6a75d1f722db0777b0e7b8ffc
sri <sri@eistut.com>
2011-06-24 19:07:43 -0400

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eistut::Application.config.secret_token = '5329cbff6c7c1df451feaf7d17f457e74d9fa88b5fde19ab02ffa24aae748c1b0378b874ed7a7394e8199bd096938ad6132224780ec2a6907cf511fe31a3287f'
